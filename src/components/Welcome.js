5fe04888ac1e9ef2ab4eadcd5ed6c7c6197487ef
shriramrathod470 <shriramrathod47@gmail.com>
2022-09-10 11:05:35 +0530

    edite4

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,7 @@ return
 (
     <div>
             {/* <h1>hello world</h1> */}
-            <h1>Hey !{props.name} </h1> 
+            <h1>"Hey!"{props.name} </h1> 
             <h1>"Welcome to Newton School."</h1> 
   
     </div>
